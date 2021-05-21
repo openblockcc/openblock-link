da1413d2e14820ba44c17806c31b09899ba9dfd0
ArthurZheng <869705086@qq.com>
2021-05-21 13:17:48 +0800

    adjust the firmware name

--- src/lib/firmware.js
+++ src/lib/firmware.js
@@ -1,14 +1,16 @@
 const firmware = {
     // Arduino UNO
-    'arduino:avr:uno': 'arduinoUno.standardFirmata.hex',
+    'arduino:avr:uno': 'arduinoUno.standardFirmata.ino.hex',
+    // Arduino UNO Ultra (Customized uno whitch has A6 A7 pins)
+    'arduino:avr:unoUltra': 'arduinoUnoUltra.standardFirmata.ino.hex',
     // Arduino Nano
-    'arduino:avr:nano:cpu=atmega328': 'arduinoUno.standardFirmata.hex',
+    'arduino:avr:nano:cpu=atmega328': 'arduinoUno.standardFirmata.ino.hex',
     // Arduino Mini
-    'arduino:avr:mini:cpu=atmega328': 'arduinoUno.standardFirmata.hex',
+    'arduino:avr:mini:cpu=atmega328': 'arduinoUno.standardFirmata.ino.hex',
     // Arduino Leonardo
-    'arduino:avr:leonardo': 'arduinoLeonardo.standardFirmata.hex',
+    'arduino:avr:leonardo': '',
     // Arduino Mega 2560
-    'arduino:avr:mega:cpu=atmega2560': 'arduinoMega2560.standardFirmata.hex'
+    'arduino:avr:mega:cpu=atmega2560': 'arduinoMega2560.standardFirmata.ino.hex'
 };
 
 module.exports = firmware;
