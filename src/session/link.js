b76034d8ec95b3a127626cbf33b96c9063934c5e
OttawaSTEM <ottawastem@gmail.com>
2021-07-11 11:30:08 -0400

    Finished link status

--- src/session/link.js
+++ src/session/link.js
@@ -3,27 +3,18 @@ const Session = require('./session');
 class StatusSession extends Session {
     constructor (socket) {
         super(socket);
-        this.services = null;
     }
 
     async didReceiveCall (method, params, completion) {
         switch (method) {
             case 'status':
-                this.status();
+                this.sendRemoteRequest('status', {
+                    result: 'ok'
+                });
                 completion(null, null);
                 break;
             default:
                 throw new Error(`Method not found`);
-        }
-    }
-
-    status () {
-        if (this.services) {
-            throw new Error('Get status error when connected');
-        } else {
-            this.sendRemoteRequest('status', {
-                result: 'ok'
-            });
         }
     }
 }
