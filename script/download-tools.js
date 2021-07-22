12a3ca709a23338a1ee268e0e5a7a01cab0497be
ArthurZheng <869705086@qq.com>
2021-07-22 13:56:58 +0800

    add ia32 fetch cmd

--- script/download-tools.js
+++ script/download-tools.js
@@ -10,7 +10,23 @@ const leaveZipped = false;
 
 const filterRelease = release => release.prerelease === false;
 
-const filterAsset = asset => (asset.name.indexOf(os.platform()) >= 0) && (asset.name.indexOf(os.arch()) >= 0);
+const parseArgs = function () {
+    const scriptArgs = process.argv.slice(2); // remove `node` and `this-script.js`
+    let arch = null;
+
+    for (const arg of scriptArgs) {
+        const archSplit = arg.split(/--arch(\s+|=)/);
+        if (archSplit.length === 3) {
+            arch = archSplit[2];
+        }
+    }
+    return arch;
+};
+
+const arch = parseArgs() || os.arch();
+
+const filterAsset = asset => (asset.name.indexOf(os.platform()) >= 0) &&
+    (asset.name.indexOf(arch) >= 0);
 
 if (!fs.existsSync(outputdir)) {
     fs.mkdirSync(outputdir, {recursive: true});
